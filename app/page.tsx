391a954aad88dbeddbf8d7f73dfdf53278483792
VidStandMedia <vidstandmedia@gmail.com>
2026-05-16 19:16:48 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">
+          VidStand Media
+        </h1>
+
+        <p className="mt-4 text-xl">
+          We help creators get more YouTube views with Google Ads.
+        </p>
+
+        <button className="mt-6 px-6 py-3 bg-black text-white rounded-xl">
+          Get Started
+        </button>
+      </div>
+    </main>
+  );
+}
